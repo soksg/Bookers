12b359638df6b4e14f080b6a0bb4758f7b2fa076
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-06 06:57:22 +0000

    [Add]3-12 edit作成。要エラー処理

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,13 @@ class BooksController < ApplicationController
   # end
 
   def create
-    book=Book.find(params[:id])
+    book=Book.new(book_params)
     if book.save
       redirect_to book_path(book.id)
       flash[:notice] = "Book was successfully created."
-    else
-      flash[:alert] = "Book was successfully created."
+    # 作成途中
+    # else
+    #   flash[:alert] = ""
     end
   end
 
@@ -42,7 +43,7 @@ class BooksController < ApplicationController
 
  private
   def book_params
-    params.requier(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
