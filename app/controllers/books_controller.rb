688ee22155360a528df7db9a62344d0296307295
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-06 02:45:42 +0000

    [Add]3-12 RSpecインストール完了,index.html.erb仮入力

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,15 +11,19 @@ class BooksController < ApplicationController
   def create
     book=Book.find(params[:id])
     if book.save
-      flash[:notice] = "Book was successfully created."
       redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      flash[:alert] = "Book was successfully created."
     end
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     book=Book.find(params[:id])
+    if book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(book.id)
+    end
   end
 
   def show
