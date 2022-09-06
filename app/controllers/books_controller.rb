34f1926a7cfcd51a419e28de959fd92183d36298
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-09-06 07:42:36 +0000

    [Add]3-12 フラッシュメッセージ含め、destroyアクションを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,7 +38,9 @@ class BooksController < ApplicationController
   def destroy
     book=Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to "/books"
+
   end
 
  private
